669924cc954601db3a16131ad78c3810ce858f18
vinayaachar <vinayaachar@gmail.com>
2021-03-13 12:34:49 -0800

    Add test for the second table

--- server/spec/test.js
+++ server/spec/test.js
@@ -1,7 +1,7 @@
 var assert = require('chai').assert;
 var expect = require('chai').expect
 var { exampleDataGenerator } = require('../example.data');
-
+var { exampleEnrolledGenerator } = require('../example.data');
 
 (function() {
   describe('Seeding Tests', function() {
@@ -18,7 +18,14 @@ var { exampleDataGenerator } = require('../example.data');
       it('Seeding generator return array of length inputted by user', function() {
         var titleArray = exampleDataGenerator(5);
         expect(titleArray[0].titleName.length).to.equal(5);
-      })
-    })
+      });
+    });
+
+    describe('returns a number for total Enrolled', function() {
+      it('Seeding generator return a number for total enrolled in a course', function() {
+        var totalEnrolled = exampleEnrolledGenerator();
+        expect(typeof totalEnrolled).to.equal('number');
+      });
+    });
   });
 }());
