2bf7bf7abcdca20fc6204a82e05a3755d499f5e2
jasonschreib <jwschreiber1@gmail.com>
2021-05-13 13:03:06 -0700

    Working on postgres Seed and refactoring file structure for routes

---
+++ db/postgresSeed.js
@@ -0,0 +1,17 @@
+//*This module contains the seeding function of the postgres db - TODO : create command to seed the db in package.json
+
+//import the client to access the postgres db
+
+//import the data generation module
+
+//seeding function
+
+//create a table in the db that I want
+
+//set a new array var equal to the invocation of data gen function with 10 million records
+
+//iterate over this array
+
+//create new entry for each item in the array
+
+//invoke the seeding function
