b8950220b20f87cd634984154963edb68717ed9e
jasonschreib <jwschreiber1@gmail.com>
2021-05-22 16:30:15 -0700

    Implemented postgres update function successfully

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -30,87 +30,104 @@ var cassandra = require('cassandra-driver');
 module.exports = {
   title: {
     post: async function (req, res) {
+      //create variables for the id and title
+      const id = req.body.id;
+      const title = req.body.title;
+
       //if the db is postgres
       if (process.env.ENV_DB === 'pg') {
-        //create variables for the id and title
-        const id = req.body.id;
-        const title = req.body.title;
         //take care of duplicates - if the id in the req exists already
         try {
           var query = await pgClient.query(`SELECT title FROM titles WHERE id = ${id}`);
-            //if the id is already found
-            if (query.rows.length !== 0) {
-              //send response that item already exists
-              res.json(`${id} already exists in db`);
-              console.log(`${id} already exists in db`);
-              //otherwise
-            } else {
-              //query the database to insert a new title, given an input title and input id
-              try {
-                await pgClient.query(`INSERT INTO titles (id, title, enrolled) VALUES (${id}, '${title}', 0)`);
-                res.status(200).json(`Added id ${id} with title '${title}'`);
-              } catch (err) {
-                res.status(400).send(err);
-                console.log('Error inserting into db', err);
-              };
-            }
+          //if the id is already found
+          if (query.rows.length !== 0) {
+            //send response that item already exists
+            res.json(`${id} already exists in db`);
+            console.log(`${id} already exists in db`);
+            //otherwise
+          } else {
+            //query the database to insert a new title, given an input title and input id
+            try {
+              await pgClient.query(`INSERT INTO titles (id, title, enrolled) VALUES (${id}, '${title}', 0)`);
+              res.status(200).json(`Added id ${id} with title '${title}'`);
+            } catch (err) {
+              res.status(400).send(err);
+              console.log('Error inserting into db', err);
+            };
+          }
           //if there is an error
         } catch (err) {
           res.status(400).send(err);
           console.log('searching for id not successful', err);
         }
-      } else if(process.env.ENV_DB === 'cass') {
+      } else if (process.env.ENV_DB === 'cass') {
 
       }
     },
-get: async function (req, res) {
-  //if the env db is postgres
-  if (process.env.ENV_DB === 'pg') {
-    //query the database for the specific id
-    try {
-      const results = await pgClient.query(`SELECT title FROM titles WHERE id = ${req.params.id}`);
-      res.status(200).json(results.rows[0].title);
-    } catch (err) {
-      res.status(400).send(err);
-      console.log('item not found');
+    get: async function (req, res) {
+      //if the env db is postgres
+      if (process.env.ENV_DB === 'pg') {
+        //query the database for the specific id
+        try {
+          const results = await pgClient.query(`SELECT title FROM titles WHERE id = ${req.params.id}`);
+          res.status(200).json(results.rows[0].title);
+        } catch (err) {
+          res.status(400).send(err);
+          console.log('item not found');
+        }
+      }
+      //otherwise if the env db is cass
+      else if (process.env.ENV_DB === 'cass') {
+
+      }
+    },
+    update: async function (req, res) {
+      //create variables for the id and title
+      const id = req.params.id
+      const title = req.body.title;
+      //if the env db is postgres
+      if (process.env.ENV_DB === 'pg') {
+        pgClient.query(`UPDATE titles SET title = '${title}' WHERE id = ${id}`)
+          .then(() => {
+            //query the database for the specific id
+              pgClient.query(`SELECT title FROM titles WHERE id = ${id}`)
+              .then((results) => res.status(200).json(results.rows[0].title))
+              .catch(err => {
+                res.status(400).send(err);
+              console.log(`error selecting item with id ${id}`, err);
+              });
+          })
+          .catch(err => {
+            res.status(400).send(err);
+            console.log(`Error updating id ${id}`, err);
+          })
+        //otherwise if the env db is cass
+      } else if (process.env.ENV_DB === 'cass') {
+
+      }
+
+    },
+    delete: function (req, res) {
+      //if the env db is postgres
+      if (process.env.ENV_DB === 'pg') {
+
+        //otherwise if the env db is cass
+      } else if (process.env.ENV_DB === 'cass') {
+
+      }
     }
-  }
-  //otherwise if the env db is cass
-  else if (process.env.ENV_DB === 'cass') {
-
-  }
-},
-update: function (req, res) {
-  //if the env db is postgres
-  if (process.env.ENV_DB === 'pg') {
-
-    //otherwise if the env db is cass
-  } else if (process.env.ENV_DB === 'cass') {
-
-  }
-
-},
-delete: function (req, res) {
-  //if the env db is postgres
-  if (process.env.ENV_DB === 'pg') {
-
-    //otherwise if the env db is cass
-  } else if (process.env.ENV_DB === 'cass') {
-
-  }
-}
   },
 
-enrolled: {
-  get: function (req, res) {
-    //if the env db is postgres
-    if (process.env.ENV_DB === 'pg') {
+  enrolled: {
+    get: function (req, res) {
+      //if the env db is postgres
+      if (process.env.ENV_DB === 'pg') {
 
-      //otherwise if the env db is cass
-    } else if (process.env.ENV_DB === 'cass') {
+        //otherwise if the env db is cass
+      } else if (process.env.ENV_DB === 'cass') {
 
+      }
     }
   }
-}
 
 }
