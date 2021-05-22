bb3b30bfdc6adeee509d3c2b6d5968e59ced8865
jasonschreib <jwschreiber1@gmail.com>
2021-05-22 15:37:43 -0700

    Removed console logs from pg post function

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -38,10 +38,8 @@ module.exports = {
         //take care of duplicates - if the id in the req exists already
         try {
           var query = await pgClient.query(`SELECT title FROM titles WHERE id = ${id}`);
-          console.log('Query', query);
             //if the id is already found
             if (query.rows.length !== 0) {
-              // console.log('Exists', exists);
               //send response that item already exists
               res.json(`${id} already exists in db`);
               console.log(`${id} already exists in db`);
@@ -49,9 +47,8 @@ module.exports = {
             } else {
               //query the database to insert a new title, given an input title and input id
               try {
-                console.log('in the try');
-                const insert = await pgClient.query(`INSERT INTO titles (id, title, enrolled) VALUES (${id}, '${title}', 0)`);
-                res.status(200).json(insert);
+                await pgClient.query(`INSERT INTO titles (id, title, enrolled) VALUES (${id}, '${title}', 0)`);
+                res.status(200).json(`Added id ${id} with title '${title}'`);
               } catch (err) {
                 res.status(400).send(err);
                 console.log('Error inserting into db', err);
